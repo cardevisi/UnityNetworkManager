a8fe58d4ff4f3ec3a40b6372f5a2b282bde947d9
Cardevisi <cardevisi@github.com>
2014-04-22 23:45:21 -0300

    Unity Files

---
+++ Assets/NetworkManager/scripts/Client.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Client : MonoBehaviour {
+
+	public Transform playerPrefab;
+	private bool serverStarted = true;
+
+	void CreatePlayer2D () {
+		Transform playerClone = Network.Instantiate (playerPrefab, gameObject.transform.position, gameObject.transform.rotation, 0) as Transform;
+		// Cria uma cor randomica para o jogador e envia via RPC para todos conectados neste momento.
+		playerClone.networkView.RPC("ChangeColor", RPCMode.All,Random.Range(0,255),Random.Range(0,255),Random.Range(0,255));
+		// Atualiza o nome do jogador para todos conectados neste momento.
+		playerClone.networkView.RPC("ChangePlayerName", RPCMode.All,GameObject.Find("PlayerNameGUIText").guiText.text);
+		
+	}
+
+	void OnConnectedToServer() {
+		Debug.Log ("OnConnectedToServer");
+		CreatePlayer2D();
+	}
+	
+	void OnServerInitialized() {
+		Debug.Log ("OnServerInitialized");
+		CreatePlayer2D();
+	}
+	
+	void ConnectToServer(HostData data) {
+		Debug.Log ("DATA"+data);
+	}
+	
+	
+	void OnDisconnectedFromServer() {
+		//oldMainCamera.gameObject.SetActive(true);
+	}
+}
---
+++ Assets/NetworkManager/scripts/NetworkManager.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	int serverPort = 8632;
+	string registerGameName = "MASTER_SERVER_TEST";
+	//bool isRefreshing = false;
+	float refreshRequestLenght = 3.0f;
+	HostData[] hostData;
+	bool connected = false;
+
+
+	private void StartServer() {
+		Network.InitializeServer(16, serverPort, true);
+		//MasterServer.RegisterHost(registerGameName, "SpaceShipGameServer", "First room on server");
+	}
+
+	void OnServerInitialized() {
+		MasterServer.RegisterHost(registerGameName, "SpaceShipGameServer", "First room on server");
+		Debug.Log("Server has been initialied!");
+		connected = true;
+	}
+
+	void OnConnectedToServer() {
+		connected = true;
+	}
+
+	void OnDisconnectedFromServer(NetworkDisconnection info) {
+		if (Network.isServer)
+			Debug.Log("Local server connection disconnected");
+		else if (info == NetworkDisconnection.LostConnection)
+				Debug.Log("Lost connection to the server");
+		else
+			Debug.Log("Successfully diconnected from the server");
+
+
+		connected = false;
+	}
+
+	void OnMasterServerEvent(MasterServerEvent masterServerEvent) {
+		if (masterServerEvent == MasterServerEvent.RegistrationSucceeded) {
+			Debug.Log ("Registration successfull!");
+		}
+	}
+
+	public IEnumerator RefreshHostList() {
+		Debug.Log ("Refreshing...");
+		MasterServer.RequestHostList(registerGameName);
+		float timeStarted = Time.time;
+		float timeEnd = timeStarted + refreshRequestLenght;
+
+		while (Time.time < timeEnd) {
+			hostData = MasterServer.PollHostList ();
+			yield return new WaitForEndOfFrame();
+		}
+
+		if (hostData == null || hostData.Length == 0) {
+				Debug.Log ("No active servers have been found");
+		} else {
+				Debug.Log (hostData.Length + " have been found!");
+		}
+
+	}
+
+	void OnGUI () {
+
+		print ("COPNE"+connected);
+
+		if (Network.isServer)
+			GUILayout.Label("Running as a server");
+		else if (Network.isClient)
+			GUILayout.Label("Running as a client");
+
+
+			if (!connected) {
+
+			/*if (Network.isClient || Network.isServer) {
+				return;
+			}*/
+
+				if (GUI.Button (new Rect (25f, 25f, 150f, 30f), "Start new Server")) {
+						StartServer ();
+				}
+
+				if (GUI.Button (new Rect (25f, 65f, 150f, 30f), "Refresh Server List")) {
+						StartCoroutine ("RefreshHostList");
+				}
+
+				if (hostData != null) {
+						for (int i = 0; i < hostData.Length; i++) {
+								if (GUI.Button (new Rect (Screen.width / 2, 65f + (30f * i), 300f, 30f), hostData [i].gameName)) {
+										Network.Connect ("127.0.0.1", serverPort);
+										print ("yay" + NetworkPeerType.Client);
+								}
+						}
+				}
+
+		} else {
+				GUILayout.Label ("Connections: " + Network.connections.Length.ToString ());
+		}
+
+	}
+}
